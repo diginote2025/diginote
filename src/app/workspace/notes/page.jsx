00d51591f9e601179c403567154363a33b4dd8b1
rajurao <rajurao1107@gmail.com>
2025-08-07 23:49:51 +0530

    commit changes

--- src/app/workspace/notes/page.jsx
+++ src/app/workspace/notes/page.jsx
@@ -812,7 +812,7 @@ const NotesPageFlip = () => {
                           return pages.map((pageContent, pageIndex) => (
                             <div
                               key={`${chapter}--${topic}--page-${pageIndex}`}
-                              className={`flex flex-col h-full rounded-2xl justify-between p-4 sm:p-5 md:p-6 ${
+                              className={`flex flex-col h-full lg:rounded-2xl rounded-r-2xl justify-between p-4 sm:p-5 md:p-6 ${
                                 isDark
                                   ? "bg-gray-800 text-white"
                                   : "bg-white text-gray-800"
