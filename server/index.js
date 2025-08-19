2d92739907fc3de3c908892dac6ba211717aed84
rajurao <rajurao1107@gmail.com>
2025-08-19 13:34:22 +0530

    commit changes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors"
 const app = express();
 const port = 3022;
+
+app.use(cors())
 
 app.get("/diginote/test", (req, res) => {
   res.send("hello world");
