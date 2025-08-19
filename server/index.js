27ba1970dd0b523d5521136aee53277be18e78fd
rajurao <rajurao1107@gmail.com>
2025-08-19 12:51:17 +0530

    commit changes

---
+++ server/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 3022;
+
+app.post("/diginote/test", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on http://localhost:${port}/diginote/test`);
+});
